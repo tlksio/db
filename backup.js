66e6e613e5a4f392be362e97d9f62aafec8500d0
Raül Pérez <repejota@gmail.com>
2015-02-04 01:25:14 +0100

    Initial source code

---
+++ backup.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+var config = require('../front/config.json');
+
+MongoClient.connect(config.mongodb, function (err, db) {
+    if (err) { throw new Error(err); }
+    var talks = db.collection('talks');
+    talks.find({}).toArray(function (err, docs) {
+        if (err) { throw new Error(err); }
+        docs.forEach(function (el) {
+            var outputFilename = "./data/talks/"+el.id+".json";
+            fs.writeFile(outputFilename, JSON.stringify(el, null, 4), function(err) {
+                if(err) { throw new Error(err); }
+            });
+        });
+        db.close();
+    });
+});
