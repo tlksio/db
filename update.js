d61fd775ed075dae01448c0ec3026344c8e72fd7
Raül Pérez <repejota@gmail.com>
2015-02-07 18:08:05 +0100

    Batch update talks

---
+++ update.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+var config = require('../front/config.json');
+
+function updateTalk(talk) {
+    if (talk.created === undefined) {
+        talk.created = Date.now();
+    }
+    talk.updated = Date.now();
+    return talk;
+}
+
+MongoClient.connect(config.mongodb, function (err, db) {
+    if (err) { throw new Error(err); }
+    var talks = db.collection('talks');
+    talks.find({}).toArray(function (err, docs) {
+        if (err) { throw new Error(err); }
+        docs.forEach(function (talk) {
+            console.log("Updated talk "+talk.id);
+            talk = updateTalk(talk);
+            talks.update({id: talk.id}, talk, function (error) {
+                if (err) { throw new Error(err); }
+                db.close();
+            });
+        });
+    });
+});
