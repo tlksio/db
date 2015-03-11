1cabb969615da43d0ca0d8f38543c33526fadb2b
Raül Pérez <repejota@gmail.com>
2015-03-11 01:44:23 +0100

    Added AUTHORS file

--- backup.js
+++ backup.js
@@ -3,15 +3,21 @@ var MongoClient = require('mongodb').MongoClient;
 
 var config = require('../front/config.json');
 
-MongoClient.connect(config.mongodb, function (err, db) {
-    if (err) { throw new Error(err); }
+MongoClient.connect(config.mongodb, function(err, db) {
+    if (err) { 
+        throw new Error(err);
+    }
     var talks = db.collection('talks');
-    talks.find({}).toArray(function (err, docs) {
-        if (err) { throw new Error(err); }
-        docs.forEach(function (el) {
-            var outputFilename = "./data/talks/"+el.id+".json";
+    talks.find({}).toArray(function(err, docs) {
+        if (err) {
+            throw new Error(err);
+        }
+        docs.forEach(function(el) {
+            var outputFilename = "./data/talks/" + el.id + ".json";
             fs.writeFile(outputFilename, JSON.stringify(el, null, 4), function(err) {
-                if(err) { throw new Error(err); }
+                if (err) {
+                    throw new Error(err);
+                }
             });
         });
         db.close();
