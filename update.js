1549374c3991dc43b30d9024644fc97245597928
Raül Pérez <repejota@gmail.com>
2015-04-01 03:06:03 +0200

    Clean up update.js

--- update.js
+++ update.js
@@ -8,8 +8,7 @@ function updateTalk(talk) {
     if (talk.created === undefined) {
         talk.created = Date.now();
     }
-    talk.updated = Date.now();
-    talk.tags = talk.tags.map(function (el) { return el.trim(); });
+    //talk.updated = Date.now();
     return talk;
 }
 
