5d527da9c13a0fadee92cc4fbeebf0e1fa46ee25
Raül Pérez <repejota@gmail.com>
2015-04-02 20:43:15 +0200

    Started import and update from files

--- import.js
+++ import.js
@@ -1,12 +1,20 @@
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 
-var config = require('../front/config.json');
+var config = require('./config.json');
+var data_path = "./data/talks/";
 
-MongoClient.connect(config.mongodb, function (err, db) {
+MongoClient.connect(config.mongodb, function(err, db) {
     if (err) {
         throw new Error(err);
     }
+    // TODO: Remove old documents, remove collection
+    // TODO: Create collection
     var talks = db.collection('talks');
+    var files = fs.readdirSync(data_path);
+    files.forEach(function(file_path) {
+        var talk = require(talk_path);
+        console.log("Import : " + talk.title);
+    });
     db.close();
 });
