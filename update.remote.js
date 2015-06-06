21fc316277c63ad7a06940503285feff77a69d5c
Raül Pérez <raul.perez@r3labs.io>
2015-06-06 16:32:29 +0200

    Backup Sat  6 Jun 2015 16:32:29 CEST

--- update.remote.js
+++ update.remote.js
@@ -11,6 +11,13 @@ function updateTalk(talk) {
     return talk;
 }
 
+function uniq(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
+
 MongoClient.connect(config.mongodb, function (err, db) {
     if (err) { 
         throw new Error(err);
@@ -22,16 +29,19 @@ MongoClient.connect(config.mongodb, function (err, db) {
         }
         docs.forEach(function (talk) {
             talk = updateTalk(talk);
-            /*
-            talks.update({
-                "id": talk.id
-            }, talk, function (error) {
-                if (err) {
-                    throw new Error(err);
-                }
-                db.close();
-            });
-            */
+            var pre = talk.tags;
+            talk.tags = uniq(talk.tags);
+            if (pre.length!=talk.tags.length) {
+                console.log(talk.tags.toString());
+                talks.update({
+                    "id": talk.id
+                }, talk, function (error) {
+                    if (err) {
+                        throw new Error(err);
+                    }
+                    db.close();
+                });
+            }
         });
     });
 });
